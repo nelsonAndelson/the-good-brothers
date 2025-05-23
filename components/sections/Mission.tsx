134093a45ee8af16cd6f9b4c843cf82dd6c0ff34
nelsonAndelson <nelsonbaguma15@gmail.com>
2025-05-23 10:51:29 -0400

    removed import react

--- components/sections/Mission.tsx
+++ components/sections/Mission.tsx
@@ -1,5 +1,4 @@
 'use client';
-import React from 'react';
 import Image from 'next/image';
 
 const Mission = () => {
@@ -34,8 +33,6 @@ const Mission = () => {
       position: 'object-center'
     },
   ];
-
-
 
   return (
     <section className="w-full py-16 bg-white">
